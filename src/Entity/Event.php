40b510b5e509429451d3b463ffeee0f1198de1be
mmoyaerts <mathieu.moyaerts.pro@gmail.com>
2024-02-19 19:43:43 +0100

    create entity event

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -4,5 +4,77 @@ namespace App\Entity;
 
 class Event
 {
+	private int $id;
+	private string $name;
+	private string $description;
+	private string $startDate;
+	private string $endDate;
+	private string $tag;
+	private int $capacity;
+	private int $ownerId;
 
+	public function getId()
+	{
+		return $this->id;
+	}
+	private function setId(int $id)
+	{
+		$this->id = $id;
+	}
+	public function getName()
+	{
+		return $this->name;
+	}
+	public function setName(string $name)
+	{
+		$this->name = $name;
+	}
+	public function getDescription()
+	{
+		return $this->description;
+	}
+	public function setDescription(string $description)
+	{
+		$this->description = $description;
+	}
+	public function getStartDate()
+	{
+		return $this->startDate;
+	}
+	public function setStartDate(string $startDate)
+	{
+		$this->startDate = $startDate;
+	}
+	public function getEndDate()
+	{
+		return $this->endDate;
+	}
+	public function setEndDate(string $endDate)
+	{
+		$this->endDate = $endDate;
+	}
+	public function getTag()
+	{
+		return $this->tag;
+	}
+	public function setTag(string $tag)
+	{
+		$this->tag = $tag;
+	}
+	public function getCapacity()
+	{
+		return $this->capacity;
+	}
+	public function setCapacity(int $capacity)
+	{
+		$this->capacity = $capacity;
+	}
+	public function getOwnerId()
+	{
+		return $this->ownerId;
+	}
+	public function setOwnerId(int $ownerId)
+	{
+		$this->ownerId = $ownerId;
+	}
 }
