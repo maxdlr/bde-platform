90a9d0dc929df20439349561da57de5a947c765e
mmoyaerts <mathieu.moyaerts.pro@gmail.com>
2024-02-23 09:52:32 +0100

    send mail on change date

--- src/Service/Mail/MailManager.php
+++ src/Service/Mail/MailManager.php
@@ -2,6 +2,7 @@
 
 namespace App\Service\Mail;
 
+use App\Repository\ParticipantRepository;
 use App\Repository\UserRepository;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -49,12 +50,12 @@ class MailManager
         $startDate = $event->getStartDate();
         $strStartDate = $startDate->format("Y-m-d H:i:s");
         $emailto = $user->getEmail();
-        $subject = "Confirmation de votre inscription";
-        $message = "Bonjour ". $user->getFirstName() . " " . $user->getLastName() . ",\n\n";
-        $message .= "Nous sommes ravis de vous compter parmi les participants inscrits à notre prochain événement, ". $event->getName() . "";
-        $message .= "Date : ".$strStartDate . "\n";
-        $message .= "Votre inscription a été enregistrée avec succès et nous sommes impatients de vous accueillir pour partager cette expérience enrichissante.\n\n";
-        $message .= "Cordialement,\nVotre équipe d'organisation";
+        $subject = "Confirmation de votre inscription ";
+        $message = "Bonjour ". $user->getFirstName() . " " . $user->getLastName() . ",\n\n ";
+        $message .= "Nous sommes ravis de vous compter parmi les participants inscrits à notre prochain événement, ". $event->getName() . " ";
+        $message .= "Date : ".$strStartDate . "\n  ";
+        $message .= "Votre inscription a été enregistrée avec succès et nous sommes impatients de vous accueillir pour partager cette expérience enrichissante.\n\n ";
+        $message .= "Cordialement,\nVotre équipe d'organisation du BDE";
         $this->sendMail($emailto, $subject, $message);
     }
 
@@ -64,11 +65,11 @@ class MailManager
             if($userRepository->executeRequest("UPDATE user SET token = '$token' WHERE email = '$emailto'"))
 
                 $subject = "Confirmation de votre compte";
-                $message = "Bonjour,\n\n";
-                $message .= "Merci de vous être inscrit sur notre site.\n";
-                $message .= "Veuillez cliquer sur le lien suivant pour activer votre compte :\n";
-                $message .= "http://localhost:8000/user/validate/" . $token . "\n";
-                $message .= "Cordialement,\nVotre BDE";
+                $message = "Bonjour,\n\n ";
+                $message .= "Merci de vous être inscrit sur notre site.\n ";
+                $message .= "Veuillez cliquer sur le lien suivant pour activer votre compte :\n ";
+                $message .= "http://localhost:8000/user/validate/" . $token . "\n ";
+                $message .= "Cordialement,\n Votre BDE";
 
                 $this->sendMail($emailto, $subject, $message);
         }
@@ -79,6 +80,29 @@ class MailManager
             $userForValidate->setIsVerified(true);
             $userRepository->update(["isVerified" => "1"], ["token" => $token]);
  
+        }
+
+        public function sendModifDate($event, $startDate)
+        {
+            $strStartDate = $startDate->format("Y-m-d H:i:s");
+
+            $subject = "Changement de date !";
+            $message = "Bonjour,\n\n" .
+            "Nous tenions à vous informer qu'il y a eu un changement de date pour l'événement \"" . $event->getName() . "\". La nouvelle date est le ".$strStartDate ." Veuillez noter ce changement dans votre agenda.\n\n
+            Nous vous prions de nous excuser pour tout inconvénient que cela pourrait causer.\n\n".
+            "Cordialement,\n" .
+            "L'équipe d'organisation du BDE";
+            $participantRepository = new ParticipantRepository;
+            $participants = $participantRepository->findBy(["event_id" => $event->getId()]);
+            $userRepository = new UserRepository;
+            foreach ($participants as $participant)
+            {
+                $userId = $participant->getUserId();
+                $user = $userRepository->findOneBy(["id" => $userId]);
+                $email = $user->getEmail();
+
+                $this->sendMail($email, $subject, $message);
+            }
         }
 
 }
